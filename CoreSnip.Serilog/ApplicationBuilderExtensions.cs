cd6fd7dbcd4a3e753eec0ba95663a01858d09e53
Cesare De Sanctis <cesare.desanctis@hotmail.it>
2021-05-25 19:46:18 +0200

    Custom request/response Serilog Enricher added

---
+++ CoreSnip.Serilog/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+namespace CoreSnip.Serilog
+{
+    /// <summary>
+    /// <see cref="IApplicationBuilder"/> extension methods to add custom middlewares
+    /// </summary>
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the custom <see cref="RequestLoggingScopeEnricherMiddleware"/> middleware and the the Serilog Request Logging 
+        /// enriching the diagnostic context with the HTTP Request and Response body and headers. <see cref="SerilogLoggerHelper.EnrichFromResponse(IDiagnosticContext, Microsoft.AspNetCore.Http.HttpContext)"/>
+        /// <para>The middleware adds the fallowing value to the logging scope/Serilog diagnostic context:</para>
+        /// RequestBody, RequestHeaders, RequestMethod, User, ResponseBody, ResponseHeaders
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseSerilogRequestResponseLogging(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestLoggingScopeEnricherMiddleware>();
+            app.UseSerilogRequestLogging(opts => opts.EnrichDiagnosticContext = SerilogEnrichHelper.EnrichFromResponse);
+        }
+    }
+}
---
+++ CoreSnip.Serilog/SerilogEnrichHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CoreSnip.Serilog
+{
+    /// <summary>
+    /// Serilog helper to enrich the <see cref="IDiagnosticContext"/> with response body and headers
+    /// </summary>
+    public class SerilogEnrichHelper
+    {
+        /// <summary>
+        /// Adds <see cref="HttpContext.Response"/> Body and Headers to the <see cref="IDiagnosticContext"/>
+        /// </summary>
+        /// <param name="diagnosticContext"></param>
+        /// <param name="httpContext"></param>
+        public static async void EnrichFromResponse(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            string responseBody = await ReadResponseBody(httpContext.Response);
+            diagnosticContext.Set("ResponseBody", responseBody);
+            diagnosticContext.Set("ResponseHeaders", JsonSerializer.Serialize(httpContext.Response.Headers));
+        }
+
+        private static async Task<string> ReadResponseBody(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            string responseBody = await new StreamReader(response.Body).ReadToEndAsync();
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return responseBody;
+        }
+    }
+}
